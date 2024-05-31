2eeaf6c8ad2064f0a45f7893db6351f3839c896e
Jillian Meehan <jillian@lickability.net>
2024-05-31 18:00:39 -0400

    Update quartz.config.ts

--- quartz.config.ts
+++ quartz.config.ts
@@ -22,8 +22,8 @@ const config: QuartzConfig = {
       fontOrigin: "googleFonts",
       cdnCaching: true,
       typography: {
-        header: "Schibsted Grotesk",
-        body: "Source Sans Pro",
+        header: "Libre Baskerville",
+        body: "Inter",
         code: "IBM Plex Mono",
       },
       colors: {
